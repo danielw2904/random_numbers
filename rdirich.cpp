981c1fe9bb44d71edc4b0e9bf3fc3351d3ba95d3
Daniel Winkler <danielw2904@disroot.org>
2019-02-02 17:52:20 +0100

    removed rstudio comment

--- rdirich.cpp
+++ rdirich.cpp
@@ -94,10 +94,6 @@ arma::mat rd_boost(int n, arma::rowvec alpha) {
   x.each_col() /= V;
   return x;
 }
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
-// run after the compilation.
-//
 
 /*** R
 # From
